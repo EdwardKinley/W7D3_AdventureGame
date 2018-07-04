b74aae17eb78653c94763acb0940af3ed3d2b4cd
Can Toraman <cantoraman@gmail.com>
2018-07-04 14:39:20 +0100

    room has players

--- src/main/java/Room.java
+++ src/main/java/Room.java
@@ -1,2 +1,24 @@
+import java.util.ArrayList;
+
 public class Room {
+
+
+    private int treasures;
+    private int number;
+
+    private Enemy enemy;
+    private ArrayList<Player> players;
+
+    public Room(int number, Enemy enemy, ArrayList<Player> players, int treasures) {
+        this.number = number;
+        this.enemy = enemy;
+        this.players = players;
+        this.treasures = treasures;
+
+    }
+
+    public int getPlayerCount() {
+        return players.size();
+    }
+
 }
---
+++ src/test/java/RoomTest.java
@@ -0,0 +1,40 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
+public class RoomTest {
+
+    Room room;
+    Enemy enemy;
+    ArrayList<Player> players;
+    Dwarf dwarf;
+    Knight knight;
+    Cleric cleric;
+
+    @Before
+    public void setup()
+    {
+
+        enemy = new Enemy(RaceType.TROLL);
+        dwarf = new Dwarf("Can", 100);
+        knight = new Knight("Greg", 100);
+        cleric = new Cleric("Ed", 100);
+        players = new ArrayList<>();
+        players.add(dwarf);
+        players.add(knight);
+        players.add(cleric);
+
+        room = new Room(1, enemy, players, 20);
+
+
+    }
+
+    @Test
+    public void hasPlayers(){
+        assertEquals(3, room.getPlayerCount());
+    }
+
+}
